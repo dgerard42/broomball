f1b96c032d846d934753a4fa40ab76d9ae42e5b3
dgerard42 <daniella4422@gmail.com>
2020-02-13 13:24:15 -0800

    I made it actually good and unbuggy

--- src/Broomball.java
+++ src/Broomball.java
@@ -12,19 +12,19 @@
 
 import objectdraw.*;
 
-public class Broomball extends WindowController{
+public class Broomball extends WindowController {
 
     private static final int CANVAS_HEIGHT = 500;
     private static final int CANVAS_WIDTH = 500;
     private static final int DIRT_SIZE = 30;
     private static final int BROOM_SIZE = 10;
-    private static final int OFFSET = DIRT_SIZE / 2;
+    private static final int OFFSET = (DIRT_SIZE / 2) - (DIRT_SIZE / 5);
 
     private FilledOval broom;
     private FilledRect dirt;
     private Location dirtStart = new Location(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
 
-    public void sweep(){
+    public void sweep() {
 
         int broomX = broom.getX() + BROOM_SIZE / 2;
         int broomY = broom.getY() + BROOM_SIZE / 2;
@@ -32,16 +32,23 @@ public class Broomball extends WindowController{
         int dirtY = dirt.getY() + DIRT_SIZE / 2;
 
 
-        if (broomX > dirtX && broomY < dirtY + OFFSET && broomY > dirtY - OFFSET)
+        if (broomX > dirtX && broomY < dirtY + OFFSET && broomY > dirtY - OFFSET) {
             dirt.move(-1, 0);
-        else if (broomY > dirtY && broomX < dirtX + OFFSET && broomX > dirtX - OFFSET)
+        } else if (broomY > dirtY && broomX < dirtX + OFFSET && broomX > dirtX - OFFSET) {
             dirt.move(0, -1);
-        else if (broomY < dirtY && broomX < dirtX + OFFSET && broomX > dirtX - OFFSET)
+        } else if (broomY < dirtY && broomX < dirtX + OFFSET && broomX > dirtX - OFFSET) {
             dirt.move(0, 1);
-        else if (broomX < dirtX && broomY < dirtY + OFFSET && broomY > dirtY - OFFSET)
+        } else if(broomX<dirtX && broomY < dirtY +OFFSET &&broomY >dirtY -OFFSET) {
             dirt.move(1,0);
-        else
-            dirt.move(1,1);
+        } else if (broomX > dirtX && broomY < dirtY) {
+            dirt.move(-1, 1);
+        } else if (broomY > dirtY && broomX < dirtX) {
+            dirt.move(1, -1);
+        } else if (broomY < dirtY && broomX < dirtX) {
+            dirt.move(1, 1);
+        } else {
+            dirt.move(-1,-1);
+        }
     }
 
     public void begin(){
